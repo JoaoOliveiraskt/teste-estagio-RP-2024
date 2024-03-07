4656a54e7f099d624562f3f3f0e3093fa98001a0
João Victor Oliveira <joaoliveiraa7@gmail.com>
2024-03-07 13:57:44 -0300

    add exercise questions

--- ex-01.js
+++ ex-01.js
@@ -1,8 +1,22 @@
+/*
+1) Observe o trecho de código abaixo:
+
+int INDICE = 13, SOMA = 0, K = 0;
+enquanto K < INDICE faça
+{
+K = K + 1;
+SOMA = SOMA + K;
+}
+imprimir(SOMA);
+
+Ao final do processamento, qual será o valor da variável SOMA?
+*/
+
 const indice = 13;
 let soma = 0;
 let k = 0;
 
-for(k; k < indice; k++) {
+for (k; k < indice; k++) {
   soma += k;
 }
 
